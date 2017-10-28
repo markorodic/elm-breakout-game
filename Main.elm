43bb22630af64c2505a4edc13f6cfde1dd547f65
Marko Rodic <mrmarkorodic@googlemail.com>
2017-10-28 23:39:32 +0100

    install svg package and draw paddle

--- Main.elm
+++ Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (program, text)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 main =
-    text "Hello World"
+    svg
+        [ viewBox "0 0 100 100", width "300px" ]
+        [ rect [ width "20", height "2", x "40", y "90" ] []
+        ]
