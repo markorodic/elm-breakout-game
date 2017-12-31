138e3987e93dc8a735f48ba88a7a61414e1220de
markorodic <mrmarkorodic@googlemail.com>
2017-12-31 15:34:29 +0000

    Refactor ball position unit test
    The test for ballPosition is checking whether the velocity is being added to the ball position, but only at one point on the screen. Change this to a fuzz test to check all possible ball positions.

--- tests/BallTests.elm
+++ tests/BallTests.elm
@@ -2,18 +2,23 @@ module BallTests exposing (ballUpdates)
 
 import Constants exposing (..)
 import Expect exposing (..)
+import Fuzz exposing (Fuzzer)
 import Model exposing (..)
-import Test exposing (Test, describe, test)
+import Test exposing (Test, describe, fuzz, test)
 
 
 ballUpdates : Test
 ballUpdates =
     describe "Test ball updates"
-        [ test "Add velocity to ball position" <|
-            \() ->
-                { model | ballPosition = { x = 0, y = 0 }, ballVelocity = { x = 2, y = 2 } }
+        [ fuzz someFuzzer "fuzz test" <|
+            \ballPosition ->
+                let
+                    updatedPosition =
+                        { x = ballPosition.x + 4, y = ballPosition.y + 4 }
+                in
+                { model | ballPosition = ballPosition, ballVelocity = { x = 4, y = 4 } }
                     |> updateBallPosition
-                    |> Expect.equal { model | ballPosition = { x = 2, y = 2 }, ballVelocity = { x = 2, y = 2 } }
+                    |> Expect.equal { model | ballPosition = updatedPosition, ballVelocity = { x = 4, y = 4 } }
         , test "Change ball y velocity to positive on left wall collision" <|
             \() ->
                 { model | ballPosition = { x = 0, y = 100 }, ballVelocity = { x = -4, y = 4 } }
@@ -50,3 +55,17 @@ ballUpdates =
                     |> updateBallVelocity
                     |> Expect.equal { model | ballPosition = { x = 200, y = 400 }, ballVelocity = { x = 0, y = 0 } }
         ]
+
+
+someFuzzer : Fuzzer Ball
+someFuzzer =
+    Fuzz.map2 serializePosition
+        (Fuzz.intRange 0 400)
+        (Fuzz.intRange 0 399)
+
+
+serializePosition : Int -> Int -> Ball
+serializePosition xPos yPos =
+    { x = xPos
+    , y = yPos
+    }
